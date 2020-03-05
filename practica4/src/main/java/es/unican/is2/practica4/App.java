4e4197291f1b934441d4e55a5a2b273972ec5d1b
fjavhoz <fjavhoz@gmail.com>
2020-03-05 11:12:59 +0100

    Creada Practica 4 y Maven

---
+++ practica4/src/main/java/es/unican/is2/practica4/App.java
@@ -0,0 +1,13 @@
+package es.unican.is2.practica4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
