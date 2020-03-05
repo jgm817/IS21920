c194b0ba0f7d046b67ead40ee440e3ae44d78b47
fjavhoz <fjavhoz@gmail.com>
2020-03-05 12:34:39 +0100

    Modificado Empleado

--- practica4/src/main/java/es/unican/is2/model/Empleado.java
+++ practica4/src/main/java/es/unican/is2/model/Empleado.java
@@ -1,24 +1,47 @@
 package es.unican.is2.model;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 public class Empleado {
 
 	private Categoria tipoEmpleado;
 	private String nombre;
-	private Date fechaContratacion;
+	private LocalDate fechaContratacion;
 	private boolean baja;
 	
-	public Empleado(Categoria tipoEmpleado, String nombre, Date fechaContratacion, boolean baja) {
+	public Empleado(Categoria tipoEmpleado, String nombre, LocalDate fechaContratacion) {
 		this.tipoEmpleado=tipoEmpleado;
 		this.nombre=nombre;
 		this.fechaContratacion=fechaContratacion;
-		this.baja=baja;
+		this.baja=false;
 	}
 	
-	public double sueldoBruto(Date antiguedad, boolean baja) {
+	public double sueldoBruto() {
 		double sueldoFinal=0;
+		switch(this.tipoEmpleado) {
 		
+		case OBRERO:
+			sueldoFinal+=1000;
+		case GESTOR:
+			sueldoFinal+=1200;
+		case DIRECTIVO:
+			sueldoFinal+=1500;
+		}
+		
+		
+		if(fechaContratacion.minusYears(5).getYear()>0) {
+			sueldoFinal+=50;
+		}
+		
+		else if(fechaContratacion.minusYears(5).getYear()>0) {
+			sueldoFinal+=100;
+
+		}
+		
+		else if(fechaContratacion.minusYears(5).getYear()>0) {
+			sueldoFinal+=200;
+
+		}
 		
 		return sueldoFinal;
 	}
