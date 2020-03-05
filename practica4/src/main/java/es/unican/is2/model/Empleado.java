54b34fad579169a937007773e9b975423337ebd8
fjavhoz <fjavhoz@gmail.com>
2020-03-05 11:48:25 +0100

    Cambiada estructura de paquetes

--- practica4/src/main/java/es/unican/is2/model/Empleado.java
+++ practica4/src/main/java/es/unican/is2/model/Empleado.java
@@ -8,7 +8,14 @@ public class Empleado {
 	private String nombre;
 	private Date fechaContratacion;
 	private boolean baja;
-	 
+	
+	public Empleado(Categoria tipoEmpleado, String nombre, Date fechaContratacion, boolean baja) {
+		this.tipoEmpleado=tipoEmpleado;
+		this.nombre=nombre;
+		this.fechaContratacion=fechaContratacion;
+		this.baja=baja;
+	}
+	
 	public double sueldoBruto(Date antiguedad, boolean baja) {
 		double sueldoFinal=0;
 		
